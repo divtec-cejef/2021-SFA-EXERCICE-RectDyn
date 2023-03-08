534477408d87bc435eabfa54f1e27ea9766a591b
Steve Fallet <steve.fallet@divtec.ch>
2023-03-08 11:58:29 +0100

    simplification js de base

--- script.js
+++ script.js
@@ -6,9 +6,5 @@
  * http://usejsdoc.org/
  */
 
-(function main() {
-    'use strict'; // Demande un interprétation stricte du code
-
-    console.log("Votre code ici !");
-
-}()); // Main IIFE
+'use strict'; // Demande un interprétation stricte du code
+console.log("Coucou !");
